48e18b55226ce843c86ac88de789609152765583
nagshaik <naginhussain.shaik@gmail.com>
2025-12-16 13:46:57 +0530

    Adjust S3 bucket policy for ALB logs: allow PutObjectAcl with bucket-owner-full-control

--- module/alb-access-logs-malawi.tf
+++ module/alb-access-logs-malawi.tf
@@ -66,8 +66,16 @@ resource "aws_s3_bucket_policy" "malawi_alb_access_logs" {
         Principal = {
           Service = "logdelivery.elasticloadbalancing.amazonaws.com"
         }
-        Action   = "s3:PutObject"
+        Action   = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
         Resource = "${aws_s3_bucket.malawi_alb_access_logs.arn}/eks-alb/AWSLogs/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
       },
       {
         Sid      = "AWSLogDeliveryAclCheck"
